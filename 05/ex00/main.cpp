c85eee5df04bc479ece8ffe69723e14f19f9ac43
sinryuji <siler2474@gmail.com>
2023-01-07 13:35:52 +0900

    05 ex00 done

--- 05/ex00/main.cpp
+++ 05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hyeongki <hyeongki@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 16:04:05 by hyeongki          #+#    #+#             */
-/*   Updated: 2023/01/06 16:16:36 by hyeongki         ###   ########.fr       */
+/*   Updated: 2023/01/07 13:29:51 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(void)
 {
 	try {
-//		Bureaucrat b1 = Bureaucrat();
+		Bureaucrat b1 = Bureaucrat();
 		Bureaucrat b2 = Bureaucrat("b2", 151);
 		std::cout << "in?" << std::endl;
 	} catch (std::exception& e) {
@@ -23,12 +23,12 @@ int	main(void)
 	}
 
 	try {
-//		Bureaucrat b3 = Bureaucrat("b3", 1);
-//		Bureaucrat b4 = Bureaucrat("b4", 100);
-//		b4.increaseGrade();
-//		std::cout << b4 << std::endl;
-//		b3.increaseGrade();
-//		std::cout << b3 << std::endl;
+		Bureaucrat b3 = Bureaucrat("b3", 1);
+		Bureaucrat b4 = Bureaucrat("b4", 100);
+		b4.increaseGrade();
+		std::cout << b4 << std::endl;
+		b3.increaseGrade();
+		std::cout << b3 << std::endl;
 	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
